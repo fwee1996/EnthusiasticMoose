ef1b1550ff7c4560b6606ce5a7ce475f7f811357
Fiona Wee <fwee@oswego.edu>
2024-07-15 12:31:22 -0400

    moose asks multiple questions

--- Program.cs
+++ Program.cs
@@ -18,16 +18,74 @@ Console.WriteLine(); //create extra line break in between welcome and picture
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
 
-// Ask a question
-bool isTrue = MooseAsks("Is Canada real?");
-//Console.WriteLine(isTrue);
-if (isTrue)
+// // Ask a question:
+// bool isTrue = MooseAsks("Is Canada real?");
+// //Console.WriteLine(isTrue);
+// if (isTrue)
+// {
+//     MooseSays("Really? It seems very unlikely.");
+// }
+// else
+// {
+//     MooseSays("I  K N E W  I T !!!");
+// }
+
+// Ask multiple questions:
+CanadaQuestion();
+EnthusiasticQuestion();
+LoveCSharpQuestion();
+SecretQuestion();
+
+void CanadaQuestion()
 {
-    MooseSays("Really? It seems very unlikely.");
+    bool isTrue = MooseAsks("Is Canada real?");
+    if (isTrue)
+    {
+        MooseSays("Really? It seems very unlikely.");
+    }
+    else
+    {
+        MooseSays("I  K N E W  I T !!!");
+    }
 }
-else
+
+void EnthusiasticQuestion()
 {
-    MooseSays("I  K N E W  I T !!!");
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
+}
+
+void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+void SecretQuestion()
+{
+    bool wantsSecret = MooseAsks("Do you want to know a secret?");
+    if (wantsSecret)
+    {
+        MooseSays("ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
+    }
 }
 
 //Create a MooseSays() function that displays moose:
