5329ea414433ec74326f7c51218e00cab62dfd1f
Fiona Wee <fwee@oswego.edu>
2024-07-15 11:43:54 -0400

    moose speaks messages

--- Program.cs
+++ Program.cs
@@ -4,4 +4,52 @@
 
 using System;
 
-Console.Write("Welcome to the Enthusiastic Moose Simulator! \n --------------------------------------------");
+//Console.Write("Welcome to the Enthusiastic Moose Simulator! \n --------------------------------------------");
+
+//Or:
+Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+Console.WriteLine("--------------------------------------------");
+Console.WriteLine(); //create extra line break in between welcome and picture
+
+//Call the fn!
+//MooseSays();
+
+// Let the moose speak! Call the fn with messages:
+MooseSays("H I, I'M  E N T H U S I A S T I C !");
+MooseSays("I really am enthusiastic");
+
+//Create a MooseSays() function that displays moose:
+//@ in front of the string to allow for a multi-line string.
+//$ character to the front of the string to enable string interpolation.
+void MooseSays(string message)
+{
+    Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+    ");
+}
+
